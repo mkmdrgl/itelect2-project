112a1752c66e67ad54d21995f250b2a93f6fd259
Mika Madrigal <mikamadrigal10@gmail.com>
2026-07-15 14:45:58 +0800

    add: console log for format date in app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
  import { formatDate } from "./utils.js";
  import { validateTask } from "./utils.js";
  import { mergeTaskUpdate } from "./utils.js";
+
+ console.log (formatDate(new Date("2026-07-22")));

edfd7b1914f1272e81e1ac9cd515649cb862340c
Mika Madrigal <mikamadrigal10@gmail.com>
2026-07-15 14:48:51 +0800

    fix: format in app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
- import { formatDate } from "./utils.js";
- import { validateTask } from "./utils.js";
- import { mergeTaskUpdate } from "./utils.js";
+import { formatDate } from "./utils.js";
+import { validateTask } from "./utils.js";
+import { mergeTaskUpdate } from "./utils.js";
 
- console.log(formatDate(new Date("2026-07-22")));
+console.log(formatDate(new Date("2026-07-22")));

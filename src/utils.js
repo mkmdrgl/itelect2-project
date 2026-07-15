d320481481428dfcc34a0488714abbe39d89117f
Mika Madrigal <mikamadrigal10@gmail.com>
2026-07-15 14:43:54 +0800

    add: mergetaskupdate in utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,7 @@
 export const formatDate = (date) => `Due: ${date.toLocaleDateString}`
 
 export const validateTask = ({ title, dueDate }) => Boolean(title && dueDate);
+
+export const mergeTaskUpdate = (original, ...updates) => {
+    return updates.reduce((acc, currentUpdate) => ({...acc, ...currentupdate}), {...original});
+}

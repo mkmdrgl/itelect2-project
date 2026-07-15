89ff0f90899a114b7b9cc1d4fcbe1e839764f3f0
Mika Madrigal <mikamadrigal10@gmail.com>
2026-07-15 14:52:36 +0800

    fix: validatetask in utils.js for when task is undefined

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 export const formatDate = (date) => `Due: ${date.toLocaleDateString()}`;
 
-export const validateTask = ({ title, dueDate }) => Boolean(title && dueDate);
+export const validateTask = ({ title, dueDate } = {}) => Boolean(title && dueDate);
 
 export const mergeTaskUpdate = (original, ...updates) => {
     return updates.reduce((acc, currentUpdate) => ({...acc, ...currentupdate}), {...original});

02481fe83b6a64c65d025f342fc31305cbf7797e
Mika Madrigal <mikamadrigal10@gmail.com>
2026-07-15 14:51:23 +0800

    add: console logs for validate task, both defined and undefined, in app.js

--- src/app.js
+++ src/app.js
@@ -3,3 +3,7 @@ import { validateTask } from "./utils.js";
 import { mergeTaskUpdate } from "./utils.js";
 
 console.log(formatDate(new Date("2026-07-22")));
+
+console.log(validateTask({ title: "Itelect2 Graded Task 3", dueDate: "2026=07-22"}));
+
+console.log(validateTask());

4c1945a11723693f147d25b12069c71896c86f0f
Mika Madrigal <mikamadrigal10@gmail.com>
2026-07-15 14:56:17 +0800

    fix: capitalized letter u in currentupdate

--- src/utils.js
+++ src/utils.js
@@ -3,5 +3,5 @@ export const formatDate = (date) => `Due: ${date.toLocaleDateString()}`;
 export const validateTask = ({ title, dueDate } = {}) => Boolean(title && dueDate);
 
 export const mergeTaskUpdate = (original, ...updates) => {
-    return updates.reduce((acc, currentUpdate) => ({...acc, ...currentupdate}), {...original});
+    return updates.reduce((acc, currentUpdate) => ({...acc, ...currentUpdate}), {...original});
 }

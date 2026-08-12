dbf2b193ede9a90afaf0be41a60d1de3288349bb
Mika Madrigal <mikamadrigal10@gmail.com>
2026-08-12 08:03:49 +0800

    add: routes/index.js for express.Router() instance

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+
+import { tasks, fetchSampleUsers } from "../utils.js";
+
+const router = express.Router();
+
+let cachedUsers = [];
+
+fetchSampleUsers().then((users) => {
+  cachedUsers = users;
+});
+
+router.get("/tasks", (req, res) => {
+  res.json(tasks);
+});
+
+router.get("/tasks/:id", (req, res) => {
+  const taskId = req.params.id; 
+  const task = tasks.find((t) => t.id === taskId || t.id === Number(taskId));
+
+  if (task) {
+    res.json(task);
+  } else {
+    res.status(404).json({ error: "Task not found" });
+  }
+});
+
+router.get("/users", (req, res) => {
+  res.json(cachedUsers);
+});
+
+export default router;
